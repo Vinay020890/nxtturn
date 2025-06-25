6fa12944461f2ea26d21df5f7c53c8c7be1a7ba5
Vinay Chalakh <vinaychalakh@gmail.com>
2025-06-25 13:57:39 +0530

    chore: Final code sync before production test before media not working on production

--- Loopline/config/settings.py
+++ Loopline/config/settings.py
@@ -137,8 +137,11 @@ if IS_PRODUCTION:
         'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
         'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
     }
+
+    
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 else:
+    
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
